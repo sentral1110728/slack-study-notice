fc0acbc6ee977e3c8be248d6f5428e844dda48dd
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-09-20 14:52:50 +0900

    土日祝日の判定

--- infrastructure/modules/functions/slack_notice/src/lambda_function.py
+++ infrastructure/modules/functions/slack_notice/src/lambda_function.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 import os
-from slack import WebClient
+import jpholiday
+from datetime import datetime,timedelta,date
+from slack    import WebClient
 
 SLACK_TOKEN = os.environ["slack_token"]
 CHANNEL_ID  = os.environ["slack_channel_id"]
@@ -19,10 +21,29 @@ def post_message(text):
     
     return response
 
+
+def is_biz_day(date):
+    print("### is_biz_day")
+    if date.weekday() >= 5 or jpholiday.is_holiday(date):
+        print("土日祝日")
+        return 0
+    else:
+        print("平日")
+        return 1
+
+
 #####
 # メイン処理
 #####
 def lambda_handler(event, context):
     print(event)
-    post_message("aaa")
+    today = date.today()
+    for num in range(6):
+        week_day = today + timedelta(days=num)
+        result = is_biz_day(week_day)
+        print(result)
+        if result == 0:
+            print("slackへメッセージ送信")
+            post_message(week_day)
+
     return event
